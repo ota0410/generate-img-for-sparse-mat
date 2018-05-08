7622b37005b30016592442540af297b2876aef4f
ota0410 <s1411506@u.tsukuba.ac.jp>
2018-05-08 19:31:23 +0900

    new file:   ReadText.m

---
+++ ReadText.m
@@ -0,0 +1,17 @@
+%open the list of matrices and read them
+%check the pattern of matrix
+filename = './txt/CGdivATA.txt';
+delimiterIn = '\n';
+headerlinesIn = 1000;
+A = importdata(filename,delimiterIn,headerlinesIn);
+
+%plot the graphs
+for k = 459:headerlinesIn,
+   str = char(A(k,1));
+   plotAtA(str,1)
+%  plotAAt(str,1)
+%  plotA(str,1)
+end;
+[y,Fs] = audioread('UC.mp3');
+sound(y,Fs);
+%load handel.mat; sound(y,Fs);

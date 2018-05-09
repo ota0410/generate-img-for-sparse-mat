2d4d2778175fb50cd7a89a6b1520227cd10c4e5d
ota0410 <s1411506@u.tsukuba.ac.jp>
2018-05-09 17:13:12 +0900

    modified:   ReadText.m 	modified:   SpHistgram.m

--- ReadText.m
+++ ReadText.m
@@ -6,7 +6,7 @@ headerlinesIn = 1000;
 A = importdata(filename,delimiterIn,headerlinesIn);
 
 %plot the graphs
-for k = 459:headerlinesIn,
+for k = 1:headerlinesIn,
    str = char(A(k,1));
    plotAtA(str,1)
 %  plotAAt(str,1)
--- SpHistgram.m
+++ SpHistgram.m
@@ -10,26 +10,31 @@ function SpHisgram(filename, sym)
         plotAtA(filename);
     end
     %}
-    RGB = imread(filename);
-    M = imresize(RGB, [112,112]);
-    sz = size(M,1);
-    M = M(:,:,1);
-    %disp(M)
-    %ヒストグラム用に一次元にする
-    h = zeros(1, sz*sz);
-    for j = 1:sz
-        for k = 1:sz
-            h(sz*(j-1)+k) = M(j,k);
+    
+    D=dir('./*.jpg')
+    for i=1:length(D)     
+        RGB = imread(D(i).name);
+        M = imresize(RGB, [112,112]);
+        sz = size(M,1);
+        M = M(:,:,1);
+        %disp(M)
+        %ヒストグラム用に一次元にする
+        h = zeros(1, sz*sz);
+        for j = 1:sz
+            for k = 1:sz
+                h(sz*(j-1)+k) = M(j,k);
+            end
         end
+        % 256階色なので
+        hist(h,256);
+        %零が多いのは当たり前なので端折った
+        xlim([1 255])
+        ylim([0 6272])
+        xlabel('色の濃度分布')
+        ylabel('ドットの数')
+        fname = strsplit(D(i).name,'.');
+        fname = strcat(fname(1),'hist','.jpg');
+        C = char(fname)
+        saveas(gcf,C);
     end
-    % 256階色なので
-    hist(h,256);
-    %零が多いのは当たり前なので端折った
-    xlim([1 255])
-    xlabel('色の濃度分布')
-    ylabel('ドットの数')
-    fname = strsplit(filename,'.');
-    fname = strcat(fname(1),'hist','.jpg');
-    C = char(fname)
-    saveas(gcf,C);
 end
